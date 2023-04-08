cc107a35dac628679d0da8c7b4282cf3881449d6
leoska <leo77551@gmail.com>
2023-04-08 11:43:20 +0300

    First commit

---
+++ bsbo-0708-22/Program.cs
@@ -0,0 +1,56 @@
+﻿static void PrintArr(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item.ToString()} ");
+    }
+    Console.WriteLine();
+}
+
+static void SortBubble(int[] arr)
+{
+    int N = arr.Length;
+    bool flag = false;
+    for (int i = 0; i < N; i++)
+    {
+        flag = false;
+
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // Способ #1 (доп. память)
+                // O(9), M(1)
+                // int tmp = arr[j];
+                // arr[j] = arr[j + 1];
+                // arr[j + 1] = tmp;
+
+                // Способ #2 (больше вычислений)
+                // O(20), M(0)
+                // arr[j + 1] = arr[j + 1] + arr[j];
+                // arr[j] = arr[j + 1] - arr[j];
+                // arr[j + 1] = arr[j + 1] - arr[j];
+
+                // Способ #3 (Tuple swap)
+                // O(6 + k), M(2k)
+                (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+
+                flag = true;
+            }
+        }
+
+        if (!flag)
+        {
+            break;
+        }
+    }
+}
+
+int[] arr = new[]
+{
+    7, 6, 5, 45, 0, -2, 30, -12, 33, 52, 129, -22
+};
+
+PrintArr(arr);
+SortBubble(arr);
+PrintArr(arr);
